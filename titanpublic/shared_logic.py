aaddb474bb9f0a49c5e55a019f237557b7253fe2
T.J. Gaffney <gaffney.tj@gmail.com>
2022-09-07 22:33:49 -0500

    Make cache function

--- titanpublic/shared_logic.py
+++ titanpublic/shared_logic.py
@@ -1,6 +1,7 @@
 import functools
 import os
-from typing import Optional
+from typing import Any, Callable, Dict, Optional, Tuple
+import warnings
 
 from frozendict import frozendict
 import yaml
@@ -12,3 +13,27 @@ def get_secrets(dir: Optional[str] = None):
         secrets = yaml.load(f, Loader=yaml.Loader)
     secrets = frozendict(secrets)
     return secrets
+
+
+def cache(func):
+    """Just functools cache, but with exception handling"""
+    _cache: Dict[Tuple[Any], Callable] = dict()
+
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        nonlocal _cache
+        key = functools._make_key(args, kwargs, False)
+        if key in _cache:
+            return _cache[key]()
+        try:
+            value = func(*args, **kwargs)
+            _cache[key] = lambda: value
+            return value
+        except Exception as e:
+            def raiser():
+                nonlocal e
+                raise e
+            _cache[key] = raiser
+            raise e
+
+    return inner

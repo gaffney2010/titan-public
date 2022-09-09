9b0ba759b6c4e19f4a9770c923c57d462b46097b
T.J. Gaffney <gaffney.tj@gmail.com>
2022-09-08 23:33:47 -0500

    CriticalException type

--- titanpublic/shared_types.py
+++ titanpublic/shared_types.py
@@ -14,6 +14,19 @@ class TitanTransientException(TitanException):
 
 
 class TitanRecurrentException(TitanException):
-    """These are failures that will persist for the model/record."""
+    """These are failures that will persist for the model/record.
+    
+    This may be due to, for example, a current-season model run on the first game of
+    the season.
+
+    These failures should be recorded to the database, so that titan won't repeatedly
+    rerun.
+    """
+
+    pass
+
+
+class TitanCriticalException(TitanException):
+    """These are unexpected errors that should crash titan."""
 
     pass

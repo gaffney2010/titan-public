3b2eae15a138f2f628a6581b6d428c43ed7538f4
T.J. Gaffney <gaffney.tj@gmail.com>
2022-09-23 21:12:15 -0500

    Add ncaaf support

--- titanpublic/date_logic.py
+++ titanpublic/date_logic.py
@@ -5,48 +5,50 @@ import warnings
 from . import shared_types
 
 
+def _current_year_start(date: shared_types.Date, sport: str) -> shared_types.Date:
+    """Return a date between season containing `date` and previous season."""
+    if "ncaam" == sport:
+        cutoff = 630
+        # Find largest cutoff that's less than date.
+        year, month_day = divmod(date, 10000)
+        if month_day <= cutoff:
+            year -= 1
+
+        return year * 10000 + cutoff
+
+    if "ncaaf" == sport:
+        year = date // 10000
+        
+        return year * 10000 + 101
+
+    raise NotImplementedError(f"Sport {sport} is not supported.")
+
+
 def season_year_label(
     date: shared_types.Date,
-    cutoff: Optional[int] = None,
     sport: str = "ncaam",
 ) -> shared_types.Date:
-    if cutoff:
-        # Need to deprecate because logic may change completely for other sports.
-        warnings.warn("cutoff argument is deprecated")
-    else:
-        if sport == "ncaam":
-            cutoff = 630
-        else:
-            raise NotImplementedError(f"Sport {sport} is not supported for year model.")
+    if "ncaam" == sport:
+        cutoff = 630
+        year, month_day = divmod(date, 10000)
+        if month_day > cutoff:
+            year += 1
 
-    year, month_day = divmod(date, 10000)
-    if month_day > cutoff:
-        year += 1
-
-    return year
+        return year
+    
+    if "ncaaf" == sport:
+        year = date // 10000
+        return year
+    
+    raise NotImplementedError(f"Sport {sport} is not supported for year model.")
 
 
 def previous_years(
     date: shared_types.Date,
     years_back: int,
-    cutoff: Optional[int] = None,
     sport: str = "ncaam",
 ) -> Tuple[shared_types.Date, shared_types.Date]:
-    if cutoff:
-        # Need to deprecate because logic may change completely for other sports.
-        warnings.warn("cutoff argument is deprecated")
-    else:
-        if sport == "ncaam":
-            cutoff = 630
-        else:
-            raise NotImplementedError(f"Sport {sport} is not supported for year model.")
-
-    # Find largest cutoff that's less than date.
-    year, month_day = divmod(date, 10000)
-    if month_day <= cutoff:
-        year -= 1
-
-    en = year * 10000 + cutoff
+    en = _current_year_start(date, sport)
     st = en - 10000 * years_back
 
     return (st, en)
@@ -59,33 +61,23 @@ def previous_year(
 
 
 def current_year(
-    date: shared_types.Date, cutoff: Optional[int] = None, sport: str = "ncaam"
+    date: shared_types.Date, sport: str = "ncaam"
 ) -> Tuple[shared_types.Date, shared_types.Date]:
-    if cutoff:
-        # Need to deprecate because logic may change completely for other sports.
-        warnings.warn("cutoff argument is deprecated")
-    else:
-        if sport == "ncaam":
-            cutoff = 630
-        else:
-            raise NotImplementedError(f"Sport {sport} is not supported for year model.")
-
-    # Find largest cutoff that's less than date.
-    year, month_day = divmod(date, 10000)
-    if month_day <= cutoff:
-        year -= 1
-
-    st = year * 10000 + cutoff
+    st = _current_year_start(date, sport)
     en = st + 10000
 
     return (st, en)
 
 
 def current_year_from_season(season: int, sport: str) -> Tuple[shared_types.Date, shared_types.Date]:
-    if sport == "ncaam":
+    # Just pick some midseason date
+    if "ncaam" == sport:
         return current_year(season*10000+101, sport=sport)
-    else:
-        raise NotImplementedError(f"Sport {sport} is not supported for year model.")
+
+    if "ncaaf" == sport:
+        return current_year(season*10000+901, sport=sport)
+    
+    raise NotImplementedError(f"Sport {sport} is not supported for year model.")
 
 
 def _get_yesterday(date: shared_types.Date) -> datetime.datetime:
@@ -97,21 +89,7 @@ def _get_yesterday(date: shared_types.Date) -> datetime.datetime:
 def current_year_through_week(
     date: shared_types.Date, cutoff: Optional[int] = None, sport: str = "ncaam"
 ) -> Tuple[shared_types.Date, shared_types.Date]:
-    if cutoff:
-        # Need to deprecate because logic may change completely for other sports.
-        warnings.warn("cutoff argument is deprecated")
-    else:
-        if sport == "ncaam":
-            cutoff = 630
-        else:
-            raise NotImplementedError(f"Sport {sport} is not supported for year model.")
-
-    # Find largest cutoff that's less than date.
-    year, month_day = divmod(date, 10000)
-    if month_day <= cutoff:
-        year -= 1
-
-    st = year * 10000 + cutoff
+    st = _current_year_start(date, sport)
 
     dt = _get_yesterday(date)
     en = dt - datetime.timedelta(dt.weekday())
@@ -123,21 +101,7 @@ def current_year_through_week(
 def current_year_through_yesterday(
     date: shared_types.Date, cutoff: Optional[int] = None, sport: str = "ncaam"
 ) -> Tuple[shared_types.Date, shared_types.Date]:
-    if cutoff:
-        # Need to deprecate because logic may change completely for other sports.
-        warnings.warn("cutoff argument is deprecated")
-    else:
-        if sport == "ncaam":
-            cutoff = 630
-        else:
-            raise NotImplementedError(f"Sport {sport} is not supported for year model.")
-
-    # Find largest cutoff that's less than date.
-    year, month_day = divmod(date, 10000)
-    if month_day <= cutoff:
-        year -= 1
-
-    st = year * 10000 + cutoff
+    st = _current_year_start(date, sport)
 
     dt = _get_yesterday(date)
     en = int(dt.strftime("%Y%m%d"))

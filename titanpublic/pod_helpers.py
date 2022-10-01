9b83090a1e19e67caeeba7ed45a4b12d924df489
T.J. Gaffney <gaffney.tj@gmail.com>
2022-09-30 21:24:32 -0500

    Fix pod_helpers a bit

--- titanpublic/pod_helpers.py
+++ titanpublic/pod_helpers.py
@@ -53,39 +53,36 @@ def routing_key_resolver(id: str, sport: str, env: str, suffix: str = "") -> str
     return f"{id}-{sport}{dev_suffix}{suffix}"
 
 
-def notify_titan(
-    input_body: str, output_timestamp: int, status: str, outbound_channel: str, titan_config: TitanConfig
-) -> None:
-    # Defined below
-    global channel
+def database_resolver(sport: str, env: str) -> str:
+    dev_suffix = ""
+    if "dev" == env:
+        dev_suffix = "-dev"
 
-    output_body = " ".join(
-        [
-            input_body,
-            str(output_timestamp),
-            status,
-        ]
-    )
+    return f"{sport}{dev_suffix}"
+
+
+def notify_titan(
+    input_body: str,
+    output_timestamp: int,
+    status: str,
+    titan_config: TitanConfig,
+    channel,
+) -> None:
+    output_body = " ".join([input_body, str(output_timestamp), status,])
     channel.basic_publish(
         exchange="",
-        routing_key=routing_key_resolver(outbound_channel, titan_config.sport, titan_config.env),
+        routing_key=routing_key_resolver(
+            titan_config.outbound_channel, titan_config.sport, titan_config.env
+        ),
         body=output_body,
         properties=pika.BasicProperties(delivery_mode=2),
     )
 
 
 def process_message(
-    body: str, callback: MessageCallback, titan_config: TitanConfig
+    body: str, callback: MessageCallback, titan_config: TitanConfig, channel
 ) -> None:
-    (
-        sport,
-        model_name,
-        input_timestamp,
-        away,
-        home,
-        date,
-        neutral,
-    ) = body.split()
+    (sport, model_name, input_timestamp, away, home, date, neutral,) = body.split()
     date = int(date)
     neutral = int(neutral)
 
@@ -97,7 +94,7 @@ def process_message(
         full_msg = f"M_ERR_TAG::{model_name}:{type(err).__name__} - {body} - {str(err)}"
         # logging.error(traceback.format_exc())
         logging.error(full_msg)
-        notify_titan(body, 0, "failure", titan_config)
+        notify_titan(body, 0, "failure", titan_config, channel)
         return
     except shared_types.TitanRecurrentException as err:
         result = {"reason": type(err).__name__}
@@ -107,19 +104,19 @@ def process_message(
     except Exception as err:  # Includes TitanCriticalExceptions
         logging.error(traceback.format_exception(err))
         logging.error(f"Uncaught exception on {body}")
-        notify_titan(body, 0, "critical", titan_config)
+        notify_titan(body, 0, "critical", titan_config, channel)
         return
 
     this_game_hash = hash.game_hash(away, home, date)
     output_timestamp = pull_data.update_feature(
-        sport,
+        database_resolver(titan_config.sport, titan_config.env),
         model_name,
         this_game_hash,
         input_timestamp,
         result,
         shared_logic.get_secrets(os.path.dirname(os.path.abspath(__file__))),
     )
-    notify_titan(body, output_timestamp, "success", titan_config.outbound_channel, titan_config)
+    notify_titan(body, output_timestamp, "success", titan_config, channel)
 
 
 class RabbitChannel(object):
@@ -127,7 +124,7 @@ class RabbitChannel(object):
         # SSL Context for TLS configuration of Amazon
         def wrapped_callback(ch, method, properties, body):
             logging.info(f"Found {body}")
-            process_message(body.decode(), callback, titan_config)
+            process_message(body.decode(), callback, titan_config, self.channel)
 
         self.callback = wrapped_callback
 
@@ -151,8 +148,20 @@ class RabbitChannel(object):
     def build_connection(self):
         self.connection = pika.BlockingConnection(self.parameters)
         self.channel = self.connection.channel()
-        self.channel.queue_declare(queue=routing_key_resolver(self.titan_config.inbound_channel, self.titan_config.sport, self.titan_config.env))
-        self.channel.queue_declare(queue=routing_key_resolver(self.titan_config.outbound_channel, self.titan_config.sport, self.titan_config.env))
+        self.channel.queue_declare(
+            queue=routing_key_resolver(
+                self.titan_config.inbound_channel,
+                self.titan_config.sport,
+                self.titan_config.env,
+            )
+        )
+        self.channel.queue_declare(
+            queue=routing_key_resolver(
+                self.titan_config.outbound_channel,
+                self.titan_config.sport,
+                self.titan_config.env,
+            )
+        )
 
     @retrying.retry(wait_fixed=BIGGER_WAIT_SEC * 1000)
     def rebuild_connection(self):
@@ -168,7 +177,11 @@ def main(callback: MessageCallback, titan_config: TitanConfig) -> None:
                 # TODO: Is this the right division?
                 channel.basic_qos(prefetch_count=PREFETCH_COUNT)
                 channel.basic_consume(
-                    queue=routing_key_resolver(titan_config.outbound_channel, titan_config.sport, titan_config.env),
+                    queue=routing_key_resolver(
+                        titan_config.outbound_channel,
+                        titan_config.sport,
+                        titan_config.env,
+                    ),
                     on_message_callback=channel.callback,
                     auto_ack=True,
                 )
@@ -183,7 +196,9 @@ def main(callback: MessageCallback, titan_config: TitanConfig) -> None:
             # Don't retry
             channel.basic_qos(prefetch_count=PREFETCH_COUNT)
             channel.basic_consume(
-                queue=routing_key_resolver(titan_config.outbound_channel, titan_config.sport, titan_config.env),
+                queue=routing_key_resolver(
+                    titan_config.outbound_channel, titan_config.sport, titan_config.env
+                ),
                 on_message_callback=channel.callback,
                 auto_ack=True,
             )

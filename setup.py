f881dbf631186d03a9f3f59fe063a0403641ed6a
T.J. Gaffney <gaffney.tj@gmail.com>
2022-10-01 19:34:20 -0500

    Bump version for reasons?

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description="Helper to read titan DB",
     author="T.J. Gaffney",
     packages=["titanpublic"],
-    version="1.14.9",
+    version="1.14.10",
     install_requires=[
         "attrs==21.4.0",
         "frozendict==2.3.4",

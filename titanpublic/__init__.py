0aae60c614ff0ac5b06583a5a2995e67f109a4b9
T.J. Gaffney <gaffney.tj@gmail.com>
2022-10-11 14:36:32 -0500

    Expose pull_data_multirange

--- titanpublic/__init__.py
+++ titanpublic/__init__.py
@@ -3,4 +3,4 @@ from . import hash
 from . import pod_helpers
 from . import shared_logic
 from . import shared_types
-from .pull_data import update_feature, pull_data, pull_single_game
+from .pull_data import update_feature, pull_data, pull_data_multi_range, pull_single_game
